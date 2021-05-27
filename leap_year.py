7fce1a5bb54302c68969d5f77afd130c3b065306
Wanjing Chen <chenwan@oregonstate.edu>
2021-05-26 22:34:44 -0700

    update

--- leap_year.py
+++ leap_year.py
@@ -1,9 +1,11 @@
-value = input("Enter a leap year: ")
+value = int(input("Enter a leap year: "))
 
-def leap_year(value):
-	if(value < 0):
-		print("Invalid number")
-	elif(value % 4 == 0 and (value % 100 != 0 or value % 400 == 0)):
-		print(value) 
+def leap_year(n):
+	if(n < 0):
+		print(n, "invalid number")
+	elif(n % 4 == 0 and (n % 100 != 0 or n % 400 == 0)):
+		print(n, "is a leap year") 
 	else:
-		print("Not Leap Year")
+		print(n, "not Leap Year")
+
+leap_year(value)
