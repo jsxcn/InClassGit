c0cf1174c5d54f2430b53435f3bf002b945294bd
Wanjing Chen <chenwan@oregonstate.edu>
2021-06-13 03:28:42 -0700

    fix typo for test_leapyear.py

--- HW7/test_leapyear.py
+++ HW7/test_leapyear.py
@@ -1,12 +1,12 @@
 import unittest
-import leap_year
+import leapyear
 
 class testCaseLeapYear(unittest.TestCase):
     def test_leapyear_1(self):
-        self.assertEqual(leap_year.leapyear(2900), 0) #divisible by 4 and 100 should F
+        self.assertEqual(leapyear.leapyear(2900), 0) #divisible by 4 and 100 should F
 
     def test_leapyear_2(self):
-        self.assertEqual(leap_year.leapyear(1996), 1)
+        self.assertEqual(leapyear.leapyear(1996), 1)
 
     def test_leapyear_1(self):
-        self.assertEqual(leap_year.leapyear(0), -1)
+        self.assertEqual(leapyear.leapyear(0), -1)
