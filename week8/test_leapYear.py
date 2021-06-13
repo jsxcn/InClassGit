d6f869f8c3362262f49cb0b6b342540a28423876
Wanjing Chen <chenwan@oregonstate.edu>
2021-06-13 15:35:43 -0700

    moditified leapyear code and fixed typos for test_leapyear.py

--- week8/test_leapYear.py
+++ week8/test_leapYear.py
@@ -3,11 +3,11 @@ import leapYear
 
 class testCaseLeapYear(unittest.TestCase):
     def test_leapyear_1(self):
-        self.assertEqual(leapyear.leapyear(2900), 0)
+        self.assertEqual(leapYear.leapyear(2900), 0)
     def test_leapyear_2(self):
-        self.assertEqual(leapyear.leapyear(1996), 1)
+        self.assertEqual(leapYear.leapyear(1996), 1)
     def test_leapyear_3(self):
-        self.assertEqual(leapyear.leapyear(400), 1)
+        self.assertEqual(leapYear.leapyear(1400), 0)
 
 
 if __name__ == '__main__':
