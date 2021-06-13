aef3934a06ee410db1a5ff7b8bdb45995a45a95f
Wanjing Chen <chenwan@oregonstate.edu>
2021-06-13 02:13:00 -0700

    add Fuzzbizz File without code

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,16 +1,4 @@
 def FizzBuzz(i):
-    #for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            # print ('FizzBuzz')
-            return 'FizzBuzz'
-        elif i % 3 == 0:
-            # print ('Fizz')
-            return 'Fizz'
-        elif i % 5 == 0:
-            # print ('Buzz')
-            return 'Buzz'
-        else:
-            print(i)
-            return i
+
 
 # FizzBuzz()
