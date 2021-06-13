1fc6bfd27cb16ac84fea26a14562fa23894c2d51
Wanjing Chen <chenwan@oregonstate.edu>
2021-06-13 15:34:56 -0700

    fixed pytest

--- week8/pytest_leapYear.py
+++ week8/pytest_leapYear.py
@@ -1,7 +1,7 @@
 import pytest
 import leapYear
 
-def leapYear_passing_cases():
+def testCaseLeapYears():
     assert leapYear.leapyear(400) == 1
     assert leapYear.leapyear(1996) == 1
     assert leapYear.leapyear(1900) == 0
