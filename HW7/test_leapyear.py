ed3039ec522e3963aaa5f34c4f5ec2c95513faa3
Wanjing Chen <chenwan@oregonstate.edu>
2021-06-13 03:34:54 -0700

    final push after test went through

--- HW7/test_leapyear.py
+++ HW7/test_leapyear.py
@@ -8,5 +8,9 @@ class testCaseLeapYear(unittest.TestCase):
     def test_leapyear_2(self):
         self.assertEqual(leapyear.leapyear(1996), 1)
 
-    def test_leapyear_1(self):
-        self.assertEqual(leapyear.leapyear(0), -1)
+    def test_leapyear_3(self):
+        self.assertEqual(leapyear.leapyear(400), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
